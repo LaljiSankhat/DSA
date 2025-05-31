c25b17796ca0cf9e8656cc4253f48e8f97ba5af1
LaljiSankhat <sankhatlalji0306@gmail.com>
2025-05-31 11:14:07 +0530

    31/05/2025

---
+++ Practice/77_snakesAndLadders.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// leetcode 909. Snakes and Ladders
+
+
+int snakesAndLadders(vector<vector<int>>& board) {
+    // first get all indexes from board for traversal
+    int n = board.size(), lbl = 1;
+    vector<pair<int, int>> cells(n * n + 1);
+    vector<int> columns(n);
+    for(int i = 0; i < n; i++){
+        columns[i] = i;
+    }
+    for(int row = n - 1; row >= 0; row--){
+        for(int column : columns){
+            cells[lbl++] = {row, column};
+        }
+        reverse(columns.begin(), column.end());
+    }
+
+    vector<int> dist(n * n + 1, -1);
+    dist[1] = 0;
+    queue<int> q;
+    q.push(1);
+
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+
+        for(int next = curr + 1; next <= min(curr + 6, n * n); next++){
+            auto [row, column] = cells[next];   
+            int destination = board[row][column] != -1 ? board[row][column] : next;
+            if(dist[destination] == -1){
+                dist[destination] = dist[cur] + 1;
+                q.push(destination);
+            }
+        }
+    }
+
+
+    return dist[n * n];
+}
+
+
+
+int main(){
+    vector<vector<int>> matrix = {
+        {-1, -1, -1, -1, -1, -1},
+        {-1, -1, -1, -1, -1, -1},
+        {-1, -1, -1, -1, -1, -1},
+        {-1, 35, -1, -1, 13, -1},
+        {-1, -1, -1, -1, -1, -1},
+        {-1, 15, -1, -1, -1, -1}
+    };
+
+    cout << snakesAndLadders(matrix) << endl;
+    return 0;
+}
