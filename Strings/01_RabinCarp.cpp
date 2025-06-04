d7737c352d5729370ea59ba57cbd00df58874f88
LaljiSankhat <sankhatlalji0306@gmail.com>
2025-06-04 09:07:11 +0530

    04/06/2025

---
+++ Strings/01_RabinCarp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// leetcode 686. Repeated String Match
+
+// int repeatedStringMatch(string a, string b) {
+
+// }
+
+/*
+
+    def repeatedStringMatch(self, A, B):
+        q = (len(B) - 1) // len(A) + 1
+        for i in range(2):
+            if B in A * (q+i): return q+i
+        return -1
+
+*/
+
+
+
+int main(){
+    string a = "abcd";
+    string b = "cdabcdab";
+    return 0;
+}
