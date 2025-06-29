0c2bd10f7529bef420ba5bb917b6a8dd4957bee6
LaljiSankhat <sankhatlalji0306@gmail.com>
2025-06-29 10:30:21 +0530

    29/06/2025

--- Practice/96_noOfsubseqForSumCondition.cpp
+++ Practice/96_noOfsubseqForSumCondition.cpp
@@ -29,40 +29,10 @@ int numSubseq(vector<int>& nums, int target) {
     return count;
 }
 
-int method2(vector<int>& nums, int target){
-    int n = nums.size();
-
-    vector<pair<int, int>> numToIndex;
-    for(int i = 0; i < n; i++){
-        numToIndex.push_back({nums[i], i});
-    }
-
-    sort(numToIndex.begin(), numToIndex.end(), [](pair<int, int> &a, pair<int, int> &b){
-        return b.first > a.first;
-    });
-
-    // for(int i = 0; i < n; i++){
-    //     cout << "{" << numToIndex[i].first << " " << numToIndex[i].second << "} ";
-    // }
-    int i = 0, j = n - 1;
-    int ans = 0;
-    while(i < j){
-        if(numToIndex[i].first + numToIndex[j].first <= target){
-            ans += 1 << (abs(numToIndex[i].second - numToIndex[j].second) - 1);
-            i++;
-        } else {
-            j--;
-        }
-    }
-    if(2 * numToIndex[i].first <= target) ans += 1;
-    return ans;
-}
-
 
 int main(){
     vector<int> nums = {3, 5, 6, 7};
     int target = 9;
     cout << numSubseq(nums, target) << endl;
-    cout << method2(nums, target) << endl;
     return 0;
 }
